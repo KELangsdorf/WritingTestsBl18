bf1203f96fac4221e22f96e0902f3dfe25881934
KELangsdorf <kelangsdorf@gmail.com>
2024-04-13 14:39:43 -0400

    WritingTest1

---
+++ writing.js
@@ -0,0 +1,79 @@
+const multiply = (multi1, multi2) => {
+  if (typeof multi1 !== 'number' || typeof multi2 !== 'number') {
+    return NaN
+  }
+  return multi1 * multi2
+}
+
+//Concat is a method of an array used to merge two or more arrays.
+const concatOdds = (odds1, odds2) => {
+  //! before an Array checks the opposite of a variable's variable.
+  //!true is false, !false is true. Aka Opposites!
+  // || is logical OR, in this 
+  if (!Array.isArray(odds1) || !Array.isArray(odds2)) {
+    return []
+  }
+
+  if (!odds1.every(input => typeof input === 'number') || !odds2.every(input => typeof input === 'number')) {
+    return []
+  }
+  const OddNums = odds1.concat(odds2).filter(num => (num % 2 === 1) || num % 2 === -1).sort((a, b) => a - b)
+  const result = []
+
+  for (let num of OddNums) {
+    if (!result.includes(num)) {
+      result.push(num)
+    }
+  }
+  return result
+}
+
+class OnlineCart {
+  //constructor takes in everything that will pass in regarding the class
+  constructor(signedOn) {
+    //{} Shopping Cart is an Object
+    this.shoppingcart = {};
+    this.signedOn = signedOn
+  }
+//Item is Parameter
+  addItemToCart(item) {
+    const name = item.name;
+    if (name in this.shoppingcart) {
+        this.shoppingcart[name].quantity++;
+    } else {
+        this.shoppingcart[name] = { price: item.price, quantity: 1 };
+    }
+}
+
+  removeItemFromCart(item) {
+    const name = item.name
+    if (name in this.shoppingcart) {
+      if (this.shoppingcart[name].quantity > 1) {
+        this.shoppingcart[name].quantity--
+      } else {
+        delete this.shoppingcart[name]
+      }
+    }
+  }
+
+  checkoutCart() {
+    const checkingout = 0
+    if (Object.keys(this.shoppingcart).length) {
+      for (let item in this.shoppingcart) {
+        checkingout += (this.shoppingcart[item].price * this.shoppingcart[item].quantity)
+      }
+
+      if (!this.signedOn) {
+        return 'Are you an existing customer? If not Create an Account'
+      } else {
+        return checkingout
+      }
+    } else {
+      return 'Please add items prior to checkout'
+    }
+  }
+}
+
+
+module.exports = { multiply, concatOdds, OnlineCart }
+
---
+++ writing.test.js
@@ -0,0 +1,76 @@
+const { multiply, concatOdds, OnlineCart } = require('./writing');
+//Describe is a function within the library
+//Test is what is being tested
+//expect is the outcome expected
+
+//Test Pass
+describe('multiply', ()=>{
+    test('it multiples the numbers correctly', () => {
+        expect(multiply(1,2)).toEqual(2)
+    })
+
+    test('it will return NaN if input does not contain numbers', () => {
+        expect(multiply(1)).toEqual(NaN)
+        expect(multiply(1, 'a')).toEqual(NaN)
+        expect(multiply('a', 1)).toEqual(NaN)
+        expect(multiply('a', 'b')).toEqual(NaN)
+        expect(multiply()).toEqual(NaN)
+    })
+});
+
+//fail @ .toEqual, failed when numbers weren't sequential
+describe ('concatOdds', ()=>{
+test('The numbers filtered/returned will be odd numbers', ()=> {
+    expect(concatOdds([1, 4, 7], [-2, 6, -9])).toEqual([-9, 1, 7])
+    expect(concatOdds([3], [9])).toEqual([3, 9])
+})
+//fail @ .toEqual, failed when numbers weren't sequential
+test('The numbers returned wont be duplicates', ()=> {
+    expect(concatOdds([1, 3, 5, 7], [-1, 3, -5])).toEqual([-5, -1, 1, 3, 5, 7])
+    expect(concatOdds([3], [3])).toEqual([3])
+    expect(concatOdds([9, 9, 9], [1, 9])).toEqual([1, 9])
+})
+//Test Pass
+test('It returns nothing/empty if there are not 2 arrays OR if either has input that isnt an interger', () => {
+    expect(concatOdds([1, 3])).toEqual([])
+    expect(concatOdds()).toEqual([])
+    expect(concatOdds(['a', 1, 3], [1])).toEqual([])
+    expect(concatOdds([1, 3], ['a', 3])).toEqual([])
+})
+});
+
+
+describe ('OnlineCart', ()=> {
+//Creates a User Signed On or Not
+const onlineCartSignedOn = new OnlineCart(true)
+const onlineCartNotSignedOn = new OnlineCart(false)
+//Test to see if the user is logged in or not
+test('Is the user logged in', () => {
+    expect(onlineCartSignedOn.signedOn).toEqual(true)
+    expect(onlineCartNotSignedOn.signedOn).toEqual(false)
+    // expect(onlineCartSignedOn.OnlineCart).toEqual({})
+    // expect(onlineCartNotSignedOn.OnlineCart).toEqual({})
+
+})
+//Test to increase quantities of items in the cart
+test('Adding an item to cart increases the item to x', () => {
+    onlineCartSignedOn.addItemToCart({ name: `Videogame`, price: 70}) 
+    expect(onlineCartSignedOn.shoppingcart).toEqual({ Videogame: {price: 70, quantity: 1 }})
+    onlineCartSignedOn.addItemToCart({ name: `Videogame`, price: 70}) 
+    expect(onlineCartSignedOn.shoppingcart).toEqual({ Videogame: {price: 70, quantity: 2 }})
+})
+//Test to reduce quantities of an item 
+//Test to empty cart completely
+test('Removing an item from cart reduces the contents', () => {
+    expect(onlineCartSignedOn)
+    expect(onlineCartNotSignedOn)
+})
+//Checks to see if they are a customer
+//Prompts to become a customer
+//Prompts an error if nothing is in the cart to checkout
+test('Checking Out', () => {
+    expect(onlineCartSignedOn)
+    expect(onlineCartNotSignedOn)
+    
+})
+});
